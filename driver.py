850bd25e767289903e005c845f72776258ec2148
jesselupica <jesselupica@gmail.com>
2017-03-28 13:44:44 -0400

    edits. Trying to decrease latency

--- driver.py
+++ driver.py
@@ -3,9 +3,23 @@ import json
 import sample_player
 import sys
 import termios
+import tty
+import os
+import wave
 
 with open('mappings.json') as data_file:    
-    	sample_mappings = json.load(data_file)
+        sample_names = json.load(data_file)
+        sample_mappings = {}
+        for k, v in sample_names.iteritems():
+            sample_mappings[k] = ''
+            samp = wave.open(v, 'rb')
+            periodsize = samp.getframerate()
+            data = samp.readframes(periodsize)
+
+            while data:
+                # Read data from stdin
+                sample_mappings[k] += data
+                data = samp.readframes(periodsize)
 
 def get_ch():
     fd = sys.stdin.fileno()
@@ -19,24 +33,28 @@ def get_ch():
 
     return ch
 
-def button_listener(player):
-	while True:
+def button_listener():
+    while True:
         c = get_ch()
 
         if c in sample_mappings:
-        	sound_t = threading.Thread(target=player.play_sample, args=(sample_mappings[c]))
-        	sound_t.daemon = True
-			sound_t.start()
+            file_name = sample_names[c]
+            data = sample_mappings[c]
+            sound_t = threading.Thread(target=sample_player.play_sample, args=(file_name, data))
+            sound_t.daemon = True
+            sound_t.start()
         elif c.lower() == 'q':
-        	break
+            break
         else:
-        	print "invalid command"
+            print "invalid command"
 
     os._exit(0)
 
 if __name__ == '__main__':
 
-	player = sample_player.SamplePlayer()
-	t1 = threading.Thread(target=control_visualizer_settings, args=(player))
-	t1.daemon = True
-	t1.start()
+    t1 = threading.Thread(target=button_listener)
+    t1.daemon = True
+    t1.start()
+
+    while True:
+        pass
--- sample_player.py
+++ sample_player.py
@@ -1,38 +1,66 @@
-import alsaaudio 
 import wave
+import os
 
-class SamplePlayer:
+uname = os.uname()
+if uname[0] == 'Linux':
+    audio_lib_name = 'alsaaudio'
+else: 
+    audio_lib_name = 'pyaudio'
+audio_lib = __import__(audio_lib_name)
 
-    def __init__(self, device='default'):
-        self.device = alsaaudio.PCM(device=device)
+is_linux = uname[0] == 'Linux'
 
-       
-    def play_sample(self, f):    
-        samp = wave.open(f, 'rb')
-        
+def read(periodsize, i, frames, samp_width):
+    return frames[i*periodsize*samp_width:(i+1)*periodsize*samp_width]
+
+def play_sample(f, frames, dev='default'): 
+    if is_linux:
+        device = audio_lib.PCM(device=dev)
+    else:
+        # create an audio object
+        p = audio_lib.PyAudio()
+    
+    samp = wave.open(f, 'rb')
+
+    if is_linux:
         # Set attributes
-        self.device.setchannels(samp.getnchannels())
-        self.device.setrate(samp.getframerate())
+        device.setchannels(samp.getnchannels())
+        device.setrate(samp.getframerate())
 
         # 8bit is unsigned in wav files
         if samp.getsampwidth() == 1:
-            self.device.setformat(alsaaudio.PCM_FORMAT_U8)
+            device.setformat(audio_lib.PCM_FORMAT_U8)
         # Otherwise we assume signed data, little endian
         elif samp.getsampwidth() == 2:
-            self.device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+            device.setformat(audio_lib.PCM_FORMAT_S16_LE)
         elif samp.getsampwidth() == 3:
-            self.device.setformat(alsaaudio.PCM_FORMAT_S24_LE)
+            device.setformat(audio_lib.PCM_FORMAT_S24_LE)
         elif samp.getsampwidth() == 4:
-            self.device.setformat(alsaaudio.PCM_FORMAT_S32_LE)
+            device.setformat(audio_lib.PCM_FORMAT_S32_LE)
         else:
             raise ValueError('Unsupported format')
 
         periodsize = samp.getframerate() / 8
 
-        self.device.setperiodsize(periodsize)
-        
-        data = samp.readframes(periodsize)
-        while data:
-            # Read data from stdin
-            self.device.write(data)
-            data = samp.readframes(periodsize)
+        device.setperiodsize(periodsize)
+    else:
+        # open stream based on the wave object which has been input.
+        device = p.open(format =
+                    p.get_format_from_width(samp.getsampwidth()),
+                    channels = samp.getnchannels(),
+                    rate = samp.getframerate(),
+                    output = True)
+        periodsize = 1024
+
+    i = 0
+    data = read(periodsize, i, frames, samp.getsampwidth())
+
+    while data:
+        # Read data from stdin
+        device.write(data)
+        i += 1
+        data = read(periodsize, i, frames, samp.getsampwidth())
+
+    device.close() 
+    if not is_linux:   
+        p.terminate()
